ea9d9589db0dd3c830a9f69852533b24de8f3e11
Daniel Harvey <daniel.harvey@consbio.org>
2016-09-02 14:42:33 -0700

    Enhancements to is_number with corresponding tests.

--- parserutils/numbers.py
+++ parserutils/numbers.py
@@ -1,9 +1,15 @@
 from math import isnan, isinf
 
 
-def is_number(s):
+def is_number(num, if_bool=False):
+    """ :return: True if 's' is an actual number, or an object that represents one """
+
+    if isinstance(num, bool):
+        return if_bool
+    elif isinstance(num, int):
+        return True
     try:
-        number = float(s)
+        number = float(num)
         return not (isnan(number) or isinf(number))
     except (TypeError, ValueError):
         return False
--- parserutils/tests/number_tests.py
+++ parserutils/tests/number_tests.py
@@ -11,11 +11,25 @@ class NumberTestCase(unittest.TestCase):
     def test_is_number(self):
         """ Tests is_number with general inputs """
 
-        for bad in (None, EMPTY_BIN, EMPTY_STR, {}, [], set(), tuple(), float('+inf'), float('-inf'), float('nan')):
-            self.assertFalse(is_number(bad))
+        # Test invalid values with parameter that allows Booleans
+        invalid = (None, EMPTY_BIN, EMPTY_STR, {}, [], float('+inf'), float('-inf'), float('nan'))
+        for bad in invalid:
+            self.assertFalse(is_number(bad, if_bool=True), '"{0}" is actually a number'.format(type(bad).__name__))
 
-        for num in (0, 1.1, binary_type('2'.encode()), text_type('3'), binary_type('4.4'.encode()), text_type('5.5')):
-            self.assertTrue(is_number(num))
+        # Test invalid values without parameter that allows Booleans
+        invalid += (True, False)
+        for bad in invalid:
+            self.assertFalse(is_number(bad), '"{0}" is actually a number'.format(type(bad).__name__))
+
+        # Test valid values without parameter that allows Booleans
+        valid = (0, 1.1, binary_type('2'.encode()), text_type('3'), binary_type('4.4'.encode()), text_type('5.5'))
+        for num in valid:
+            self.assertTrue(is_number(num), '"{0}" is not a number'.format(type(num).__name__))
+
+        # Test valid values with parameter that allows Booleans
+        valid += (True, False)
+        for num in valid:
+            self.assertTrue(is_number(num, if_bool=True), '"{0}" is not a number'.format(type(num).__name__))
 
 
 if __name__ == '__main__':
