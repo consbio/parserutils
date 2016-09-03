1e29aab440d745da15b1999a70c99b3a101eff6c
Daniel Harvey <daniel.harvey@consbio.org>
2016-09-02 18:25:23 -0700

    Update version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='parserutils',
     description='A collection of performant parsing utilities',
     keywords='parser,parsing,utils,utilities,collections,dates,elements,numbers,strings,url,xml',
-    version='0.1.3',
+    version='0.2.0',
     packages=[
         'parserutils', 'parserutils.tests'
     ],
