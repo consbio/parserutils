a85494c6e9db0458ca50a59232fca508ba9619f5
Daniel Harvey <daniel.harvey@consbio.org>
2016-10-31 12:36:45 -0700

    Increment version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='parserutils',
     description='A collection of performant parsing utilities',
     keywords='parser,parsing,utils,utilities,collections,dates,elements,numbers,strings,url,xml',
-    version='0.8.1',
+    version='0.8.2',
     packages=[
         'parserutils', 'parserutils.tests'
     ],
