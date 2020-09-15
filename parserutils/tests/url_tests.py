0b6dad7530cff18e1593e6723627c389d4a97279
dharvey-consbio <daniel.harvey@consbio.org>
2020-09-15 15:01:51 -0700

    Add test coverage for URL trailing slash detection/preservation

--- parserutils/tests/url_tests.py
+++ parserutils/tests/url_tests.py
@@ -4,8 +4,8 @@ import unittest
 from parserutils.collections import reduce_value, wrap_value
 from parserutils.strings import EMPTY_BIN, EMPTY_STR
 from parserutils.urls import _urllib_parse
-from parserutils.urls import clear_cache, get_base_url, update_url_params
-from parserutils.urls import url_to_parts, parts_to_url
+from parserutils.urls import clear_cache, get_base_url, has_trailing_slash
+from parserutils.urls import update_url_params, url_to_parts, parts_to_url
 
 
 iteritems = getattr(six, 'iteritems')
@@ -29,16 +29,18 @@ class URLTestCase(unittest.TestCase):
         self.assertEqual(get_base_url([]), None)
 
         in_urls = (
-            'http://www.get_base_url.com',
-            'http://www.get_base_url.com/',
-            'http://www.get_base_url.com/test/path',
-            'http://www.get_base_url.com/test/path/',
-            'http://www.get_base_url.com?a=aaa&c=ccc&b=bbb',
-            'http://www.get_base_url.com/?a=aaa&c=ccc&b=bbb'
+            'http://www.base_url.com',
+            'http://www.base_url.com/',
+            'http://www.base_url.com/test/path',
+            'http://www.base_url.com/test/path/',
+            'http://www.base_url.com?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/test/path?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/test/path/?a=aaa&c=ccc&b=bbb',
         )
 
         # Test valid url values when include_path=False
-        out_url = 'http://www.get_base_url.com/'
+        out_url = 'http://www.base_url.com/'
         for url in in_urls:
             self.assertEqual(get_base_url(url), out_url)
 
@@ -49,6 +51,29 @@ class URLTestCase(unittest.TestCase):
         self.assertTrue(len(_parse_cache))
         clear_cache()
         self.assertFalse(len(_parse_cache))
+
+    def test_has_trailing_slash(self):
+        """ Tests has_trailing_slash with general inputs """
+
+        # Test empty url values
+        self.assertEqual(has_trailing_slash(None), False)
+        self.assertEqual(has_trailing_slash(EMPTY_BIN), False)
+        self.assertEqual(has_trailing_slash(EMPTY_STR), False)
+        self.assertEqual(has_trailing_slash([]), False)
+
+        url_results = (
+            ('http://www.base_url.com', False),
+            ('http://www.base_url.com/', True),
+            ('http://www.base_url.com/test/path', False),
+            ('http://www.base_url.com/test/path/', True),
+            ('http://www.base_url.com?a=aaa&c=ccc&b=bbb', False),
+            ('http://www.base_url.com/?a=aaa&c=ccc&b=bbb', True),
+            ('http://www.base_url.com/test/path?a=aaa&c=ccc&b=bbb', False),
+            ('http://www.base_url.com/test/path/?a=aaa&c=ccc&b=bbb', True),
+        )
+
+        for url, result in url_results:
+            self.assertEqual(has_trailing_slash(url), result)
 
     def test_update_url_params(self):
         """ Tests update_url_params with general inputs """
@@ -67,10 +92,10 @@ class URLTestCase(unittest.TestCase):
         # Test valid values with no URL params (ignore trailing slash)
 
         base_urls = (
-            'http://www.update_url_params.com',
-            'http://www.update_url_params.com/',
-            'http://www.update_url_params.com/test/path',
-            'http://www.update_url_params.com/test/path/'
+            'http://www.base_url.com',
+            'http://www.base_url.com/',
+            'http://www.base_url.com/test/path',
+            'http://www.base_url.com/test/path/'
         )
         for base_url in base_urls:
             self.assertEqual(update_url_params(base_url), base_url)
@@ -78,8 +103,8 @@ class URLTestCase(unittest.TestCase):
         # Test valid values with single value URL params
 
         full_urls = (
-            'http://www.update_url_params.com?a=aaa&c=ccc&b=bbb',
-            'http://www.update_url_params.com/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/?a=aaa&c=ccc&b=bbb',
         )
         for full_url in full_urls:
             in_url, in_params = self._parse_url(full_url)
@@ -112,8 +137,8 @@ class URLTestCase(unittest.TestCase):
         # Test valid values with multiple value URL params
 
         full_urls = (
-            'http://www.update_url_params.com?abc=a&abc=b&abc=c',
-            'http://www.update_url_params.com/?abc=a&abc=b&abc=c',
+            'http://www.base_url.com?abc=a&abc=b&abc=c',
+            'http://www.base_url.com/?abc=a&abc=b&abc=c',
         )
         for full_url in full_urls:
             in_url, in_params = self._parse_url(full_url)
@@ -150,8 +175,15 @@ class URLTestCase(unittest.TestCase):
         """ Tests update_url_params after calling get_base_url """
 
         full_urls = (
-            'http://www.update_url_params.com?a=aaa&c=ccc&b=bbb',
-            'http://www.update_url_params.com/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/test/path',
+            'http://www.base_url.com/test/path/',
+            'http://www.base_url.com:8080/test/path/',
+            'http://www.base_url.com?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com:8080/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/test/path?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/test/path/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com:8080/test/path/?a=aaa&c=ccc&b=bbb'
         )
         for full_url in full_urls:
             base_url = get_base_url(full_url)
@@ -190,11 +222,11 @@ class URLTestCase(unittest.TestCase):
         self.assertEqual(url_to_parts({}), None)
 
         simple_urls = (
-            'http://www.url_to_parts.com',
-            'http://www.url_to_parts.com/',
-            'http://www.url_to_parts.com:8080/',
-            'http://www.url_to_parts.com#a=aaa&c=ccc&b=bbb',
-            'http://www.url_to_parts.com/#a=aaa&c=ccc&b=bbb#'
+            'http://www.base_url.com',
+            'http://www.base_url.com/',
+            'http://www.base_url.com:8080/',
+            'http://www.base_url.com#a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/#a=aaa&c=ccc&b=bbb#'
         )
 
         for url in simple_urls:
@@ -209,12 +241,15 @@ class URLTestCase(unittest.TestCase):
             self.assertEqual(parts.fragment, split.fragment)
 
         complex_urls = (
-            'http://www.url_to_parts.com/test/path',
-            'http://www.url_to_parts.com/test/path/',
-            'http://www.url_to_parts.com?a=aaa&c=ccc&b=bbb',
-            'http://www.url_to_parts.com/?a=aaa&c=ccc&b=bbb',
-            'http://www.url_to_parts.com?a=aaa&c=ccc&b=bbb#x=xxx&y=yyy&z=zzz',
-            'http://www.url_to_parts.com/test/path?a=aaa&c=ccc&b=bbb#x=xxx&y=yyy&z=zzz'
+            'http://www.base_url.com/test/path',
+            'http://www.base_url.com/test/path/',
+            'http://www.base_url.com:8080/test/path/',
+            'http://www.base_url.com?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com/?a=aaa&c=ccc&b=bbb',
+            'http://www.base_url.com:8080?a=aaa&c=ccc&b=bbb#x=xxx&y=yyy&z=zzz',
+            'http://www.base_url.com/test/path?a=aaa&c=ccc&b=bbb#x=xxx&y=yyy&z=zzz',
+            'http://www.base_url.com/test/path/?a=aaa&c=ccc&b=bbb#x=xxx&y=yyy&z=zzz'
+            'http://www.base_url.com:8080/test/path/?a=aaa&c=ccc&b=bbb#x=xxx&y=yyy&z=zzz'
         )
 
         for url in complex_urls:
@@ -240,7 +275,7 @@ class URLTestCase(unittest.TestCase):
             else:
                 self.assertEqual(parts.query, {})
 
-        url = 'http://www.url_to_parts.com/test/path/again?a=aaa&c=ccc&b=bbb&a=xxx&c=yyy&b=zzz#x=aaa&y=bbb&z=ccc'
+        url = 'http://www.base_url.com:8080/test/path/again?a=aaa&c=ccc&b=bbb&a=xxx&c=yyy&b=zzz#x=aaa&y=bbb&z=ccc'
         parts = url_to_parts(url)
         split = urlsplit(url)
 
@@ -277,60 +312,63 @@ class URLTestCase(unittest.TestCase):
         # Test urls with different parameter configurations
 
         url_data = {
-            'http://one.parts_to_url.com/': {'netloc': 'one.parts_to_url.com'},
-            'http://two.parts_to_url.com/': {'scheme': 'http', 'netloc': 'two.parts_to_url.com'},
-            'http://www.parts_to_url.com/': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com', 'path': [], 'query': {}, 'fragment': ''
+            'http://one.base_url.com/': {'netloc': 'one.base_url.com'},
+            'http://two.base_url.com/': {'scheme': 'http', 'netloc': 'two.base_url.com'},
+            'http://www.base_url.com/': {
+                'scheme': 'http', 'netloc': 'www.base_url.com', 'path': [], 'query': {}, 'fragment': ''
             },
-            'http://www.parts_to_url.com:8080/': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com:8080', 'path': [], 'query': {}, 'fragment': ''
+            'http://www.base_url.com/': {
+                'scheme': 'http', 'netloc': 'www.base_url.com', 'path': [], 'query': {}, 'fragment': ''
             },
-            'http://www.parts_to_url.com:8080/?a=aaa': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com:8080',
+            'http://www.base_url.com/?a=aaa': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': [], 'query': {'a': 'aaa'}, 'fragment': ''
             },
-            'http://www.parts_to_url.com/#x=xxx&y=yyy&z=zzz': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com:8080/': {
+                'scheme': 'http', 'netloc': 'www.base_url.com:8080', 'path': [], 'query': {}, 'fragment': ''
+            },
+            'http://www.base_url.com:8080/?a=aaa': {
+                'scheme': 'http', 'netloc': 'www.base_url.com:8080',
+                'path': [], 'query': {'a': 'aaa'}, 'fragment': ''
+            },
+            'http://www.base_url.com/#x=xxx&y=yyy&z=zzz': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': [], 'query': {}, 'fragment': 'x=xxx&y=yyy&z=zzz'
             },
-            'http://www.parts_to_url.com/#x=xxx&y=yyy&z=zzz#': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com/#x=xxx&y=yyy&z=zzz#': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': [], 'query': {}, 'fragment': 'x=xxx&y=yyy&z=zzz#'
             },
-            'http://www.parts_to_url.com/test/path': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com/test/path/': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': ['test', 'path'], 'query': {}, 'fragment': ''
             },
-            'http://www.parts_to_url.com/test/path': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
-                'path': ['test', 'path'], 'query': {}, 'fragment': ''
-            },
-            'http://www.parts_to_url.com/test/path?b=bbb': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com/test/path/?b=bbb': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': ['test', 'path'], 'query': {'b': 'bbb'}, 'fragment': ''
             },
-            'http://www.parts_to_url.com/test/path?c=ccc#x=xxx&y=yyy&z=zzz': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com/test/path/?c=ccc#x=xxx&y=yyy&z=zzz': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': ['test', 'path'], 'query': {'c': 'ccc'}, 'fragment': 'x=xxx&y=yyy&z=zzz'
             },
-            'http://www.parts_to_url.com/?c=ccc&b=bbb&a=aaa': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com/?c=ccc&b=bbb&a=aaa': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': [], 'query': (('c', ['ccc']), ('b', ['bbb']), ('a', ['aaa'])), 'fragment': ''
             },
-            'http://www.parts_to_url.com/?c=ccc&b=bbb&a=aaa#x=xxx&y=yyy&z=zzz': {
-                'scheme': 'http', 'netloc': 'www.parts_to_url.com',
+            'http://www.base_url.com/?c=ccc&b=bbb&a=aaa#x=xxx&y=yyy&z=zzz': {
+                'scheme': 'http', 'netloc': 'www.base_url.com',
                 'path': [], 'query': (('c', ['ccc']), ('b', ['bbb']), ('a', ['aaa'])), 'fragment': 'x=xxx&y=yyy&z=zzz'
             },
-            'http://www.parts_to_url.com/test/path?c=ccc&b=bbb&a=aaa#x=xxx&y=yyy&z=zzz': {
+            'http://www.base_url.com/test/path/?c=ccc&b=bbb&a=aaa#x=xxx&y=yyy&z=zzz': {
                 'scheme': 'http',
-                'netloc': 'www.parts_to_url.com',
+                'netloc': 'www.base_url.com',
                 'path': ['test', 'path'],
                 'query': (('c', ['ccc']), ('b', ['bbb']), ('a', ['aaa'])),
                 'fragment': 'x=xxx&y=yyy&z=zzz'
             },
-            'http://www.parts_to_url.com/test/path/again?c=ccc&c=yyy&a=aaa&a=xxx&b=zzz&b=bbb#x=aaa&y=bbb&z=ccc': {
+            'http://www.base_url.com/test/path/again/?c=ccc&c=yyy&a=aaa&a=xxx&b=zzz&b=bbb#x=aaa&y=bbb&z=ccc': {
                 'scheme': 'http',
-                'netloc': 'www.parts_to_url.com',
+                'netloc': 'www.base_url.com',
                 'path': ['test', 'path', 'again'],
                 'query': (('c', ['ccc', 'yyy']), ('a', ['aaa', 'xxx']), ('b', ['zzz', 'bbb'])),
                 'fragment': 'x=aaa&y=bbb&z=ccc'
@@ -347,18 +385,24 @@ class URLTestCase(unittest.TestCase):
             ]
             parts = SplitResult(*valid)
 
+            stripped_url = url.rstrip('/').replace('/?', '?').replace('/#', '#')
+
             # Test with parts=dict
-            self.assertEqual(parts_to_url(data), url)
+            self.assertEqual(parts_to_url(data), stripped_url)
+            self.assertEqual(parts_to_url(data, trailing_slash=True), url)
 
             # Test with parts=SplitResult
-            self.assertEqual(parts_to_url(parts), url)
+            self.assertEqual(parts_to_url(parts), stripped_url)
+            self.assertEqual(parts_to_url(parts, trailing_slash=True), url)
 
             if len(data) == 5:
                 # Test with individual parameterized args
-                self.assertEqual(parts_to_url(**data), url)
+                self.assertEqual(parts_to_url(**data), stripped_url)
+                self.assertEqual(parts_to_url(trailing_slash=True, **data), url)
 
             # Test with a valid list of ordered args
-            self.assertEqual(parts_to_url(None, *valid), url)
+            self.assertEqual(parts_to_url(None, *valid), stripped_url)
+            self.assertEqual(parts_to_url(None, *valid, trailing_slash=True), url)
 
             # Test with an invalid list of ordered args
 
@@ -366,5 +410,9 @@ class URLTestCase(unittest.TestCase):
             invalid = [dict(d) if isinstance(d, (dict, tuple)) else d * 2 for d in valid]
 
             # Ensure invalid are corrupt, then that they are ignored
-            self.assertNotEqual(parts_to_url(None, *invalid), url)
-            self.assertEqual(parts_to_url(parts, *invalid), url)
+
+            self.assertNotEqual(parts_to_url(None, *invalid), stripped_url)
+            self.assertNotEqual(parts_to_url(None, *invalid, trailing_slash=True), url)
+
+            self.assertEqual(parts_to_url(parts, *invalid), stripped_url)
+            self.assertEqual(parts_to_url(parts, *invalid, trailing_slash=True), url)
